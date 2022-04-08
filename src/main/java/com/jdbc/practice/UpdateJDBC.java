22b5a69331b0f1a41ca260b4362611a419f9e285
Ankit <ankitthakur1998@gmail.com>
2022-04-08 10:55:34 +0530

    No error now

--- src/main/java/com/jdbc/practice/UpdateJDBC.java
+++ src/main/java/com/jdbc/practice/UpdateJDBC.java
@@ -4,10 +4,9 @@
  */
 package com.jdbc.practice;
 
-/**
- *
- * @author Avinash
- */
+import java.sql.*;
+import java.io.*;
+
 public class UpdateJDBC {
     public static void main(String[] args) {
         try{
@@ -27,10 +26,19 @@ public class UpdateJDBC {
             System.out.println("Enter the student id : ");
             int id=Integer.parseInt(br.readLine()); // Converting string from bufferedReader to integer
                 
+            PreparedStatement pstmt=con.prepareStatement(q);  // Passing the query
+            
+            pstmt.setString(1,name); //Setting the value in line 17
+            pstmt.setString(2,city);
+            pstmt.setInt(3,id);
+            
+            pstmt.executeUpdate(); //Firing the query
+            
+            System.out.println("done...........");
+            
+            con.close();
+            
                 
-        
-            
-            
         }catch(Exception e){
         e.printStackTrace(); // Kya errors hain ye pata chalega
         
